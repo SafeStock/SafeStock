28a91b59f8434d436720d983461fe0312d03955d
guiguimon200 <guilherme.brito877@sptech.school>
2025-04-02 16:29:56 -0300

    subindo projeto java para os cruds

---
+++ Back-end/safestock/safestock/src/main/java/com/example/safestock/SafestockApplication.java
@@ -0,0 +1,13 @@
+package com.example.safestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafestockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafestockApplication.class, args);
+	}
+
+}
---
+++ Back-end/safestock/safestock/src/test/java/com/example/safestock/SafestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
