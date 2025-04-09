904be86b62fdbf30d50cbc7e6ed37483cc420c9c
pedro.novaes <pedro.barros@sptech.school>
2025-04-09 18:44:56 -0300

    Atualizando e Organizando  pasta

--- Front-end/webSite/src/pages/ContateNos.jsx
+++
@@ -1,8 +0,0 @@
-export function ContateNos(){     
-    return(
-        <section id="Contate-nos" className="bg-[#ccc] w-full h-[100vh] text-[#5390e7] ">
-         <p>Contate-nos</p>
-        </section>
-     )
- 
-}
--- Front-end/webSite/src/pages/PorQueSafeStock.jsx
+++
@@ -1,8 +0,0 @@
-export function PorQueSafeStock() {
-    return(
-        <section id="Por que SafeStock" className="bg-[#ccc] w-full h-[100vh] text-[#5390e7]">
-         <p>má poq safe stock</p>
-        </section>
-     )
- 
-}
