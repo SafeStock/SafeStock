fb99424fd8c9ddc50736c89cfc02dea8301ba205
Pedro Paulo Novaes de Barros <160988685+PedroNovaees@users.noreply.github.com>
2025-04-26 04:23:55 -0300

    Revert "Subindo att"

--- Back-end/src/main/java/com/example/safestock/controller/FuncionarioController.java
+++ Back-end/src/main/java/com/example/safestock/controller/FuncionarioController.java
@@ -1,31 +1,27 @@
 package com.example.safestock.controller;
 
-
-import com.example.safestock.dto.*;
 import com.example.safestock.model.Funcionario;
 import com.example.safestock.service.FuncionarioService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("api/funcionarios")
 public class FuncionarioController {
-    @Autowired
-    private FuncionarioService funcionarioService;
+
+    private final FuncionarioService funcionarioService;
+
+    public FuncionarioController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
 
     @GetMapping
-    @SecurityRequirement(name = "Bearer")
-    public ResponseEntity<List<FuncionarioListar>> listarFuncionarios(){
-        List<FuncionarioListar> funcionariosEncontrados = this.funcionarioService.listarTodos();
-        if (funcionariosEncontrados.isEmpty()){
-            return ResponseEntity.status(204).build();
-        }
-        return ResponseEntity.status(200).body(funcionariosEncontrados);
+    public List<Funcionario> listarFuncionarios(){
+        return funcionarioService.listarFuncionarios();
     }
 
     @GetMapping("/{id}")
@@ -35,21 +31,11 @@ public class FuncionarioController {
     }
 
     @PostMapping
-    @SecurityRequirement(name = "Bearer")
-    public ResponseEntity<Void> adicionarFuncionario(@RequestBody @Valid Funcionario funcionario){
+    public ResponseEntity<Funcionario> adicionarFuncionario(@Valid @RequestBody Funcionario funcionario){
         Funcionario novaCreche = funcionarioService.salvarFuncionario(funcionario);
-        return ResponseEntity.status(201).build();
+        return ResponseEntity.ok(novaCreche);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<TokenDTO> login(@RequestBody FuncionarioLogin funcionarioLogin){
-        final  Funcionario funcionario = FuncionarioMapper.of(funcionarioLogin);
-        TokenDTO funcionarioTokenDto = this.funcionarioService.autenticar(funcionario);
-
-        return  ResponseEntity.status(200).body(funcionarioTokenDto);
-    }
-
-    
     @DeleteMapping("/{id}")
     public ResponseEntity<Funcionario> removerFuncionario(@PathVariable Long id){
         if (funcionarioService.buscarFuncionarioPorId(id).isPresent()){
--- Back-end/src/main/java/com/example/safestock/repository/FuncionarioRepository.java
+++ Back-end/src/main/java/com/example/safestock/repository/FuncionarioRepository.java
@@ -4,10 +4,6 @@ import com.example.safestock.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface FuncionarioRepository  extends JpaRepository<Funcionario, Long> {
-
-    Optional<Funcionario> findByEmail(String email);
 }
