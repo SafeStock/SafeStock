6aa00274dbfea4a1284628cec1c92dd2e9452cea
teodorosamuel <samuel.tsouza@sptech.school>
2025-04-27 06:37:47 -0300

    Subindo alterações da Tela De DashBoard Dono e Secretaria

--- Front-end/Plataforma/src/DashBoard/Moleculas/AreaKPIsDono.jsx
+++ Front-end/Plataforma/src/DashBoard/Moleculas/AreaKPIsDono.jsx
@@ -1,36 +1,46 @@
-import { DivElementKPIDonoLittle } from "../Atomos/DivElementKPIDono"
+import { DivElementKPIDonoLittleLeft } from "../Atomos/DivElementKPIDono"
 import { DivisionDivElementKPIDono } from "../Atomos/Divisions"
+import { DivElementKPIDonoBigLeft } from "../Atomos/DivElementKPIDono"
+import { DivisionDivElementKPIDonoBig } from "../Atomos/Divisions"
+import { DivElementKPIDonoLittleRight } from "../Atomos/DivElementKPIDono"
+import { DivisionDivElementKPIDonoBigRight } from "../Atomos/Divisions"
+import { DivElementKPIDonoBigRight } from "../Atomos/DivElementKPIDono"
 
 export function AreaKPIsDonoLateralEsquerda(){
 
     return(
         <div className="h-[95%] w-[56%] flex flex-col justify-center items-center bg-transparent">
             <div className="h-[30%] w-full flex flex-row justify-center items-center">
-                <DivElementKPIDonoLittle
+                <DivElementKPIDonoLittleLeft
                 ImgUrl={"/src/assets/Stonks.svg"}
                 Qtd="5"
                 Titulo="Produtos próximo ao limite de uso"
                 />
                 <DivisionDivElementKPIDono/>
-                <DivElementKPIDonoLittle
+                <DivElementKPIDonoLittleLeft
                 ImgUrl={"/src/assets/BoxKPI.svg"}
                 Qtd="15"
                 Titulo="Produtos presentes no estoque"
                 />
                 <DivisionDivElementKPIDono/>
-                <DivElementKPIDonoLittle
+                <DivElementKPIDonoLittleLeft
                 ImgUrl={"/src/assets/GetOutBox.svg"}
                 Qtd="2"
                 Titulo="Produtos retirados do estoque"
                 />
                 <DivisionDivElementKPIDono/>
-                <DivElementKPIDonoLittle
+                <DivElementKPIDonoLittleLeft
                 ImgUrl={"/src/assets/CalendarExpired.svg"}
                 Qtd="10"
                 Titulo="Produto próximo da validade"
                 />
             </div>
-            <div className="h-[70%] w-full"></div>
+
+            <div className="h-[70%] w-full flex flex-col items-center">
+                <DivElementKPIDonoBigLeft/>
+                <DivisionDivElementKPIDonoBig/>
+                <DivElementKPIDonoBigLeft/>
+            </div>
 
         </div>
     )
@@ -39,7 +49,9 @@ export function AreaKPIsDonoLateralEsquerda(){
 export function AreaKPIsLateralDireita(){
     return(
         <div className="h-[95%] w-[38%]">
-
+            <DivElementKPIDonoLittleRight/>
+            <DivisionDivElementKPIDonoBigRight/>
+            <DivElementKPIDonoBigRight/>
         </div>
     )
 
