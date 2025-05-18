a530b1c909b06dcbc63274d08ae0935bec1e935c
rayreis-r <raygrs40@gmail.com>
2025-05-17 21:58:07 -0300

    componente da sidebar funcionando de acordo com as rotas e mudança de icones

--- Front-end/Plataforma/src/Hooks/RotasApp.jsx
+++ Front-end/Plataforma/src/Hooks/RotasApp.jsx
@@ -47,8 +47,8 @@ export function RotasApp() {
         <Route path="/dashboard/dashboardlimpeza" element={<DashLimpeza />} />
         <Route path="/dashboard/historicouso" element={<HistoricoUso />} />
         <Route path="/dashboard/historicoalertas" element={<HistoricoAlertas />} />
-        <Route path="/etapa1" element={<CadastroProdutosEtapa1 formData={formData} setFormData={setFormData} />} />
-        <Route path="/etapa2" element={<CadastroProdutosEtapa2 formData={formData} setFormData={setFormData} />} />
+        <Route path="/cadastroEtapa1" element={<CadastroProdutosEtapa1 formData={formData} setFormData={setFormData} />} />
+        <Route path="/cadastroEtapa2" element={<CadastroProdutosEtapa2 formData={formData} setFormData={setFormData} />} />
         <Route path="/produtos" element={<DashPrincipalDono />} />
 
         
