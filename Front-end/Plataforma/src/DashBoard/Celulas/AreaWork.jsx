35146ee8fed6a91037ebd91dd4bb1f4eb3196651
guiguimon200 <guilherme.brito877@sptech.school>
2025-05-18 16:57:22 -0300

    alterando listar funcionarios no back e front

--- Front-end/Plataforma/src/DashBoard/Celulas/AreaWork.jsx
+++ Front-end/Plataforma/src/DashBoard/Celulas/AreaWork.jsx
@@ -5,11 +5,10 @@ export function AreaWork({ displayFuncionarios = "none", displayHistoricoAlerta 
       
   return (
     <div className="none items-center flex-col" style={{ display: displayFuncionarios, displayHistoricoAlerta, displayHistoricoUso }}>
-      <AreaSubTitle title1="Nome" title2="Sobrenome" title3="Cargo" title4="Email" title5="Telefone" />
+      <AreaSubTitle title1="Nome" title2="Cargo" title3="Email" title4="Telefone" />
       
-      <div className="mt-[3vh] h-[65vh] w-[85vw] flex flex-col items-center justify-center
-        overflow-y-auto rounded-[20px] 
-        shadow-[0_0_5px_rgba(0,0,0,0.7)] bg-[white]">
+      <div className="mt-[3vh] h-[65vh] w-[90vw] flex flex-col items-center justify-center
+        overflow-y-auto rounded-[20px] ">
         
         <UserInformation abrirModal={abrirModal} /> {/* ← Aqui! */}
 
