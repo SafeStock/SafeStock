f4ae400e5d11473fb07f1886bfb07fbf2ebb653c
pedro.novaes <pedro.barros@sptech.school>
2025-05-28 15:09:23 -0300

    Telas Alinhadas

--- Front-end/Plataforma/src/DashBoard/Moleculas/AreaTittle.jsx
+++ Front-end/Plataforma/src/DashBoard/Moleculas/AreaTittle.jsx
@@ -1,7 +1,7 @@
 export function AreaTittle({texto}) {
     return (
-        <div className="flex flex-row w-[90vw] h-[15vh] text-[#3A577B] text-[4.1vh] items-center ml-[10vw]">
-            <h2 className="w-[74.57vw] mt-[3vh]">{texto}</h2>
+        <div className="flex flex-row w-[90vw] h-[14vh] text-[#3A577B] text-[3.5vh] items-center ml-[10vw] ">
+            <h2 className=" mt-[2vh]">{texto}</h2>
         </div>
     );
 }
