b9be5a9547df7aa71f9edb3bf15443ef64033bda
teodorosamuel <samuel.tsouza@sptech.school>
2025-06-09 03:11:16 -0300

    Atualizar feito

--- Back-end/src/main/java/com/example/safestock/dto/produto/ProdutoDTO.java
+++ Back-end/src/main/java/com/example/safestock/dto/produto/ProdutoDTO.java
@@ -14,14 +14,12 @@ public class ProdutoDTO {
     @Schema(description = "Id do produto", example = "1")
     private Long id;
 
-    @Size(min = 3, max = 25)
     @Schema(description = "Nome do produto", example = "Candida")
     private String nome;
 
     @Schema(description = "Categoria do produto", example = "Chão")
     private CategoriaProduto categoriaProduto;
 
-    @Size(min = 1, max = 4)
     @Schema(description = "Quantidade do produto", example = "25")
     private int quantidade;
 
