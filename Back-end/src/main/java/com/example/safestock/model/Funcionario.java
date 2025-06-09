a85056a082ebe3da534e8a263d3ec4ac003a2c9a
rayreis-r <raygrs40@gmail.com>
2025-06-09 01:21:15 -0300

    arrumando regex no backend

--- Back-end/src/main/java/com/example/safestock/model/Funcionario.java
+++ Back-end/src/main/java/com/example/safestock/model/Funcionario.java
@@ -30,8 +30,7 @@ public class Funcionario {
     private String email;
 
     @NotBlank(message = "O campo senha não pode ser nulo")
-    @Pattern(regexp = ".*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?].*", message = "A senha deve conter pelo menos um caractere especial")
-    @Size(min = 4, message = "O campo senha deve conter no minimo 4 caracteres")
+    @Pattern(regexp = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*")
     private String senha;
 
     @NotBlank(message = "O campo telefone não pode ser nulo")
